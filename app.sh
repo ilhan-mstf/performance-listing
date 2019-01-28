cd0be479d09d5c3b7b56c2b396b0b2b631f8eb9f
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-01-28 22:52:57 +0300

    Enables multiple listing.

--- app.sh
+++ app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: $ ./app.sh frameworks.txt frameworks
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "$line"
     lighthouse $line --quiet --output json --output html
@@ -7,7 +9,8 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 done < "$1"
 
 DATE=$(date +"%Y-%m-%d")
+SUB_FOLDER=$2
 
-mkdir -p reports/$DATE
-mv ./*.html ./reports/$DATE/
-mv ./*.json ./reports/$DATE/
+mkdir -p reports/$SUB_FOLDER/$DATE
+mv ./*.html ./reports/$SUB_FOLDER/$DATE/
+mv ./*.json ./reports/$SUB_FOLDER/$DATE/
--- create_csv.py
+++ create_csv.py
@@ -3,7 +3,10 @@ import csv
 import glob
 import json
 from typing import Dict, List
+import sys
 
+
+folder = sys.argv[1]
 
 def read_file(file_name: str) -> Dict:
   with open(file_name) as f:
@@ -27,11 +30,11 @@ headers: List[str] = ['fetchTime', 'finalUrl', 'time-to-first-byte',
                       'first-contentful-paint', 'first-meaningful-paint', 'speed-index',
                       'first-cpu-idle', 'interactive', 'total-byte-weight', 'file_name']
 
-with open('reports/summary.csv', 'w') as csv_file:
+with open('reports/' + folder + '/summary.csv', 'w') as csv_file:
   writer = csv.writer(csv_file)
   writer.writerow(headers)
 
-  for file_name in glob.glob('reports/**/*.json'):
+  for file_name in glob.glob('reports/' + folder + '/**/*.json'):
     data: Dict = read_file(file_name)
     row: List = extract_fields(data, headers)
     row.append(file_name)
